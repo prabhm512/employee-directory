47208b245fed91d8275e95ba0ed1f744f23a65e7
Prabh Singh <prabhm512@gmail.com>
2021-01-12 15:31:35 +1100

    filter employee table on search feature functional

--- src/components/EmpTable.js
+++ src/components/EmpTable.js
@@ -1,48 +1,8 @@
 /* eslint-disable no-undef */
-import React, { useEffect } from 'react';
-import API from '../utils/API';
+import React from 'react';
 
 function EmpTable() {
-    const employees = [];
-    // const [empState, setEmpState] = useState(state);
-    
-    useEffect(() => {
-        // Make a GET request to the Random Users API
-        API.getRandomEmployees()
-            .then(res => employees.push(res.data.results))
-            .then(() => {
-                employees[0].forEach(el => {
 
-                    // Convert ISO date to DD/MM/YYYY
-                    let date = new Date(`${el.dob.date}`);
-                    let year = date.getFullYear();
-                    let month = date.getMonth()+1;
-                    let dt = date.getDate();
-
-                    if (dt < 10) {
-                        dt = '0' + dt;
-                    }
-                    if (month < 10) {
-                        month = '0' + month;
-                    }
-
-                    const newDate = dt+'-' + month + '-'+year;
-                   // eslint-disable-next-line react-hooks/exhaustive-deps
-                   let employeeHTML = `
-                        <tr>
-                            <td><img src="${el.picture.medium}"</td>
-                            <td>${el.name.first} ${el.name.last}</td>
-                            <td>${el.phone}</td>
-                            <td>${el.email}</td>
-                            <td>${newDate}</td>
-                        </tr>
-                    `;
-                    document.querySelector(".employees").innerHTML += employeeHTML;
-                })
-            })
-            .catch(err => console.log(err));
-    });
-    
     return (
         <div className="container-sm">
             <div className="row">
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,13 +1,86 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import API from '../utils/API';
+
+const employees = [];
 
 function Search() {
+
+    // Manage state of Search box
+    const [search, setSearch] = useState({search: ""});
+
+    // Render table with data in employees array
+    const renderTable = (empArray) => {
+        // Clear previous innerHTML before rendering new innerHTML
+        document.querySelector(".employees").innerHTML = "";
+
+        empArray.forEach(el => {
+
+            // Convert ISO date to DD/MM/YYYY
+            let date = new Date(`${el.dob.date}`);
+            let year = date.getFullYear();
+            let month = date.getMonth()+1;
+            let dt = date.getDate();
+
+            if (dt < 10) {
+                dt = '0' + dt;
+            }
+            if (month < 10) {
+                month = '0' + month;
+            }
+
+            const newDate = dt+'-' + month + '-'+year;
+            // eslint-disable-next-line react-hooks/exhaustive-deps
+            let employeeHTML = `
+                <tr>
+                    <td><img src="${el.picture.medium}"</td>
+                    <td>${el.name.first} ${el.name.last}</td>
+                    <td>${el.phone}</td>
+                    <td>${el.email}</td>
+                    <td>${newDate}</td>
+                </tr>
+            `;
+            document.querySelector(".employees").innerHTML += employeeHTML;
+        });
+    }
+
+    const filterEmployees = () => {
+        // If employees array is empty, do not run the filter
+        if (employees.length !== 0) {
+            const filteredEmpArray = employees[0].filter(el => el.name.first.includes(search.search));
+            // console.log(employees);
+            // console.log(filteredEmpArray);
+            renderTable(filteredEmpArray);
+        }
+    }
+
+    useEffect(() => {
+        // Only fill the employees array if it is empty
+        if (employees.length === 0) {
+            // Make a GET request to the Random Users API
+            API.getRandomEmployees()
+                .then(res => employees.push(res.data.results))
+                .then(() => {
+                    renderTable(employees[0]);
+                })
+                .catch(err => console.log(err));
+        } else {
+            filterEmployees();
+        }
+    })
+
     return (
         <div className="container-sm">
             <div className="row">
                 <div className="col-sm-4"></div>
                 <div className="col-sm-4">
                     <form>
-                    <input className="form-control" type="search" placeholder="Search" aria-label="Search"></input>
+                    <input className="form-control" name="searchString" type="search" placeholder="Search" aria-label="Search" value={search.search}
+                    onChange={event => {
+                        // Prevent the location from refreshing
+                        event.preventDefault();
+
+                        setSearch({search: event.target.value});
+                    }}></input>
                     </form>
                 </div>
                 <div className="col-sm-4"></div>
