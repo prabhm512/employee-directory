64b04e62f42ee979d0ec7098fc0bc6326a0afef4
Prabh Singh <prabhm512@gmail.com>
2021-01-13 11:16:54 +1100

    can sort by first & last name (A-Z)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import Jumbotron from './components/Jumbotron';
-import Search from './components/Search';
 import EmpTable from './components/EmpTable';
 
 function App() {
@@ -8,7 +7,6 @@ function App() {
   return (
     <div className="App">
       <Jumbotron></Jumbotron>
-      <Search></Search>
       <EmpTable></EmpTable>
     </div>
   );
--- src/components/EmpTable.js
+++ src/components/EmpTable.js
@@ -1,9 +1,145 @@
 /* eslint-disable no-undef */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import API from '../utils/API';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSortAlphaDown } from '@fortawesome/free-solid-svg-icons';
+
+const employees = [];
+let filteredEmpArray = [];
 
 function EmpTable() {
 
+    // Manage state of Search box
+    const [search, setSearch] = useState({search: ""});
+
+    // Render table with data in employees array
+    const renderTable = (empArray) => {
+        // Clear previous innerHTML before rendering new innerHTML
+        document.querySelector(".employees").innerHTML = "";
+
+        empArray.forEach(el => {
+
+            // Convert ISO date to DD/MM/YYYY
+            let date = new Date(`${el.dob.date}`);
+            let year = date.getFullYear();
+            let month = date.getMonth()+1;
+            let dt = date.getDate();
+
+            if (dt < 10) {
+                dt = '0' + dt;
+            }
+            if (month < 10) {
+                month = '0' + month;
+            }
+
+            const newDate = dt+'-' + month + '-'+year;
+            // eslint-disable-next-line react-hooks/exhaustive-deps
+            let employeeHTML = `
+                <tr>
+                    <td><img src="${el.picture.medium}"</td>
+                    <td>${el.name.first}</td>
+                    <td>${el.name.last}</td>
+                    <td>${el.phone}</td>
+                    <td>${el.email}</td>
+                    <td>${newDate}</td>
+                </tr>
+            `;
+            document.querySelector(".employees").innerHTML += employeeHTML;
+        });
+    }
+
+    const filterEmployees = () => {
+        // If employees array is empty, do not run the filter
+        if (employees.length !== 0) {
+            const searchStr = search.search.toLowerCase();
+            filteredEmpArray = employees[0].filter(el => el.name.first.toLowerCase().includes(searchStr) || el.name.last.toLowerCase().includes(searchStr) || el.email.toLowerCase().includes(searchStr));
+            renderTable(filteredEmpArray);
+        }
+    }
+
+    // Sort employees in alphabetical order
+    const sortEmployees = (arrIndexName) => {
+        
+        let sortedEmpArray = [];
+        let nameA, nameB;
+
+
+        if (filteredEmpArray.length !== 0 ) {
+            sortedEmpArray = filteredEmpArray.sort((a, b) => {
+                // To reuse function for sorting by first and last name
+                if (arrIndexName === 'first') {
+                    nameA = a.name.first.toLowerCase();
+                    nameB = b.name.first.toLowerCase();
+                } else {
+                    nameA = a.name.last.toLowerCase();
+                    nameB = b.name.last.toLowerCase();
+                }
+
+                if (nameA < nameB) {
+                    return -1;
+                } else if (nameA > nameB) {
+                    return 1;
+                } else {
+                    return 0;
+                }
+            });
+            renderTable(sortedEmpArray);
+        } else {
+            sortedEmpArray = employees[0].sort((a, b) => {
+                if (arrIndexName === 'first') {
+                    nameA = a.name.first.toLowerCase();
+                    nameB = b.name.first.toLowerCase();
+                } else {
+                    nameA = a.name.last.toLowerCase();
+                    nameB = b.name.last.toLowerCase();
+                }
+
+                if (nameA < nameB) {
+                    return -1;
+                } else if (nameA > nameB) {
+                    return 1;
+                } else {
+                    return 0;
+                }
+            });
+            renderTable(sortedEmpArray);
+        }
+    }
+
+    useEffect(() => {
+        // Only fill the employees array if it is empty
+        if (employees.length === 0) {
+            // Make a GET request to the Random Users API
+            API.getRandomEmployees()
+                .then(res => employees.push(res.data.results))
+                .then(() => {
+                    renderTable(employees[0]);
+                })
+                .catch(err => console.log(err));
+        } else {
+            filterEmployees();
+        }
+    });
+
     return (
+        <section>
+        <div className="container-sm">
+            <div className="row">
+                <div className="col-sm-4"></div>
+                <div className="col-sm-4">
+                    <form>
+                    <input className="form-control" name="searchString" type="search" placeholder="Search (name or email)" aria-label="Search" value={search.search}
+                    onChange={event => {
+                        // Prevent the location from refreshing
+                        event.preventDefault();
+
+                        setSearch({search: event.target.value});
+                    }}></input>
+                    </form>
+                </div>
+                <div className="col-sm-4"></div>
+            </div>
+        </div>
         <div className="container-sm">
             <div className="row">
                 <div className="col-sm-12">
@@ -11,8 +147,14 @@ function EmpTable() {
                         <thead>
                             <tr>
                                 <th scope="col">Image</th>
-                                <th scope="col">First Name</th>
-                                <th scope="col">Last Name</th>
+                                <th scope="col">First Name <span className="sort-alpha-down"><FontAwesomeIcon icon={faSortAlphaDown} onClick={event => {
+                                    event.preventDefault();
+                                    sortEmployees('first');
+                                }}/></span></th>
+                                <th scope="col">Last Name <span className="sort-alpha-down"><FontAwesomeIcon icon={faSortAlphaDown} onClick={event => {
+                                    event.preventDefault();
+                                    sortEmployees('last');
+                                }}/></span></th>
                                 <th scope="col">Phone</th>
                                 <th scope="col">Email</th>
                                 <th scope="col">DOB</th>
@@ -23,6 +165,7 @@ function EmpTable() {
                 </div>
             </div>
         </div>
+        </section>
     )
 }        
 
